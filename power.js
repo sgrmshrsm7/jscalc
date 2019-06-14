cb003f141ed2ff2e0b21339408f413eda89a025c
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-06-14 17:20:14 +0530

    commented 4 digit

--- power.js
+++ power.js
@@ -1,6 +1,6 @@
 function jscalc(result){
 		form.display_result.value = form.display_result.value + result;
-		if (form.display_result.value % 1 != 0){
-	form.display_result.value = form.display_result.value.toFixed(4);
-	}
+		//if (form.display_result.value % 1 != 0){
+	//form.display_result.value = form.display_result.value.toFixed(4);
+	//}
 }
